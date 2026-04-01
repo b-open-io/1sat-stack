5c0429c5d882cf82118309ab48db95d18fb35119
David Case <david.case@shruggr.cloud>
2026-04-01 14:03:22 -0400

    Remove debug log from SQLiteFactory.Topic

--- pkg/overlay/storage/sqlite.go
+++ pkg/overlay/storage/sqlite.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"log/slog"
 	"os"
 	"path/filepath"
 	"sync"
@@ -108,7 +107,6 @@ func (f *SQLiteFactory) Topic(topic string) (TopicStorage, error) {
 		return s.(*SQLiteStorage), nil
 	}
 	path := filepath.Join(f.basePath, topic+".db")
-	slog.Info("SQLiteFactory.Topic", "basePath", f.basePath, "topic", topic, "path", path)
 	s, err := NewSQLiteStorage(path)
 	if err != nil {
 		return nil, err
